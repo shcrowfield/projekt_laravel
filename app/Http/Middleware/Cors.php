e1bd320207ecdb35e52543a90de1a31b502a81da
Bgabor997 <balicsgabor@gmail.com>
2023-12-03 11:19:09 +0100

    Update Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,8 +8,7 @@ class Cors extends Middleware
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, 
-            OPTIONS')
+            ->header('Access-Control-Allow-Methods', '*')
             ->header('"Access-Control-Allow-Headers"', '*');
     }
 }
